b0fc9999a8827e7ebb13040a1d83f4df9dbb29c4
ArturT <mr.arturio@gmail.com>
2023-02-21 17:35:07 -0500

    stdin setup + exit key

--- play.js
+++ play.js
@@ -1,7 +1,23 @@
-const connect = require('./client')
+const connect = require('./client')// establishes a connection with the game server
 
-// establishes a connection with the game server
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin; // stdin object  will allow us to listen for keyboard input and react to it.
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
 
+const handleUserInput = function (key) {
+// ctrl + c to exit
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput();
 
 console.log("Connecting ...");
 connect();
