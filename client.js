2701aaacf3dcaae60fdb5e99156c3e1d1fff47a2
ArturT <mr.arturio@gmail.com>
2023-02-21 17:05:42 -0500

    move testing

--- client.js
+++ client.js
@@ -1,20 +1,31 @@
 const net = require("net");
 
-const connect = function () {
+const connect = function () { //establishing conection
   const conn = net.createConnection({
     host: 'localhost',
     port: 50541
   });
 
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
-  conn.on("connect", () => {
+
+  conn.on("connect", () => { //message after connection established
     console.log('Successfully connected to game server!');
-    conn.write('Name: ART');
+    //sending a Name to the server
+    
+        conn.write('Name: ART');
+    // setInterval(() => {
+    // conn.write("Move: left");
+    // conn.write("Move: up");
+    //  conn.write("Move: right");
+    // conn.write("Move: down");
+    // }, 100);
+   
   });
   // interpret incoming data as text
   conn.setEncoding("utf8");
+
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+     });
 
   return conn;
 };
