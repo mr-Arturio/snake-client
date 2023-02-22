7619ab6fe5b219c11d4eb145e790bc57bcfb11d7
ArturT <mr.arturio@gmail.com>
2023-02-21 20:18:18 -0500

    refactoring, input.js require

--- play.js
+++ play.js
@@ -1,23 +1,7 @@
-const connect = require('./client')// establishes a connection with the game server
-
-// setup interface to handle user input from stdin
-const setupInput = function () {
-  const stdin = process.stdin; // stdin object  will allow us to listen for keyboard input and react to it.
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-// ctrl + c to exit
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-setupInput();
+const { connect } = require('./client');// establishes a connection with the game server
+const { setupInput } = require('./input');// setup interface to handle user input from stdin
 
 console.log("Connecting ...");
 connect();
+
+setupInput();
